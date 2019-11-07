141923d3fe60976155a246cf39a769c3434a9d14
theolamide <ajadidan@gmail.com>
2019-11-07 06:36:52 -0500

    Additional Feature logic and functionality added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+import {addFeature} from './reducer/actions';
+
 const App = (props) => {
   
 
@@ -15,6 +17,7 @@ const App = (props) => {
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    props.addFeature(item)
   };
 
   return (
@@ -24,7 +27,7 @@ const App = (props) => {
         <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <AdditionalFeatures buyItem={buyItem} additionalFeatures={props.additionalFeatures} />
         <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
@@ -38,4 +41,4 @@ const mapStateToProps = (state) => {
     additionalPrice: state.additionalPrice
   }
 }
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps, {addFeature}) (App);
